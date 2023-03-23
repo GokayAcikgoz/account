e0ba1b6feccfbea63dc6400381ef1cdb9380271c
gokay <gokayacikgoz1@gmail.com>
2023-03-23 15:15:51 +0300

    initial commit

---
+++ src/main/java/com/folksdev/account/AccountApplication.java
@@ -0,0 +1,33 @@
+package com.folksdev.account;
+
+import com.folksdev.account.model.Customer;
+import com.folksdev.account.repository.CustomerRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+
+import java.time.Clock;
+import java.util.HashSet;
+
+@SpringBootApplication
+public class AccountApplication implements CommandLineRunner {
+
+	private final CustomerRepository customerRepository;
+
+	public AccountApplication(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountApplication.class, args);
+	}
+
+
+	@Override
+	public void run(String... args) throws Exception{
+		Customer customer = customerRepository.save(new Customer("","Gökay","Açıkgöz",new HashSet<>()));
+		System.out.println(customer);
+	}
+
+}
---
+++ src/main/java/com/folksdev/account/dto/TransactionDto.kt
@@ -0,0 +1,14 @@
+package com.folksdev.account.dto
+
+import com.folksdev.account.model.TransactionType
+import java.math.BigDecimal
+import java.time.LocalDateTime
+
+data class TransactionDto(
+        val id: String?,
+        val transactionType: TransactionType? = TransactionType.INITIAL,
+        val amount: BigDecimal?, //parasal işlemlerde big decimal.
+        val transactionalDate: LocalDateTime?
+) {
+
+}
